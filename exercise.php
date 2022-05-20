cac24d152bb5de753eaaf2d55b6d0956b418975c
syazwina19 <2020951065@student.uitm.edu.my>
2022-05-20 22:22:18 +0800

    enter

---
+++ exercise.php
@@ -0,0 +1,8 @@
+$monthDays = array ('Splorch' => 23, 'Sploo' => 28,
+'Splat' => v2, 'Splatt' => 3,
+'Spleen' => 44, 'Splune' => 30,
+'Spling' => 61, 'Slendo' => 61,
+
+'Sploctember' => 31, 'Splictember' => 31,
+
+'Splanet' => 30, 'TheRest' => 22);
